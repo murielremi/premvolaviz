de938a79be0d294f77f4fee49a1ab31a92ac0208
Muriel Rémi <remimanantsoa@gmail.com>
2023-10-25 13:57:46 +0300

    Added valueUnite function to the package

--- R/functions.R
+++ R/functions.R
@@ -19,3 +19,41 @@ sur_millions <- function(data){
   res <- round(data/1000000)
   return(res)
 }
+
+#' Value Unite
+#'
+#' @param val a numeric value
+#'
+#' @return string value of the input value, abbreviated with number ISU (K for Thousand, M for Million and MDS for Billion)
+#' @export
+#'
+#' @examples
+#' valueUnite(2800)
+#' output: 2.8 K
+#' valueUnite(2370000)
+#' output: 2.37 M
+#' valueUnite(7890000000)
+#' output: 7.89 MDS
+valueUnite <- function(val){
+  if(!length(val)){
+    return(0)
+  }
+  if(is.na(val)){
+    return("")
+  }
+  if(is.nan(val)){
+    return("")
+  }
+  if(val<= 1000){
+    return(paste0(val))
+  }
+  else if(val<= 1000000){
+    return(paste0(round(val/1000,2)," K"))
+  }
+  else if(val<= 1000000000){
+    return(paste0(round(val/1000000,2)," M"))
+  }
+  else{
+    return(paste0(round(val/1000000000,2)," MDS"))
+  }
+}
