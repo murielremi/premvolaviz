e22b3530e66ff93cd7c42ece9b98b1c9b64e8149
Muriel Rémi <remimanantsoa@gmail.com>
2023-10-25 14:55:36 +0300

    Added square_val function

--- R/functions.R
+++ R/functions.R
@@ -49,3 +49,17 @@ valueUnite <- function(val){
 sepMillier<-function(x){
   return(format(x, format = "f", digits = NULL, big.mark = " ", scientific=FALSE, trim=TRUE))
 }
+
+#' Square value
+#'
+#' @param x numeric value
+#'
+#' @return square value of the input
+#' @export
+#'
+#' @examples
+#' square_val(2)
+#' # output: 4
+square_val <- function(x){
+  x^2
+}
