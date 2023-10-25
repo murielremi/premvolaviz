a89f8ea2db00f6bca6a220cf136690deea0b5b59
Muriel Rémi <remimanantsoa@gmail.com>
2023-10-25 14:49:03 +0300

    substitute functions

--- R/functions.R
+++ R/functions.R
@@ -1,17 +1,3 @@
-#' Separateur de millier
-#'
-#' @param x a numeric value
-#'
-#' @return a string value of the input value with space thousand separator
-#' @export
-#'
-#' @examples
-#' sepMillier(2340000)
-#' # ouptut: 2 340 000
-sepMillier<-function(x){
-  return(format(x, format = "f", digits = NULL, big.mark = " ", scientific=FALSE, trim=TRUE))
-}
-
 #' Value Unite
 #'
 #' @param val a numeric value
@@ -48,4 +34,18 @@ valueUnite <- function(val){
   else{
     return(paste0(round(val/1000000000,2)," MDS"))
   }
+}
+
+#' Separateur de millier
+#'
+#' @param x a numeric value
+#'
+#' @return a string value of the input value with space thousand separator
+#' @export
+#'
+#' @examples
+#' sepMillier(2340000)
+#' # ouptut: 2 340 000
+sepMillier<-function(x){
+  return(format(x, format = "f", digits = NULL, big.mark = " ", scientific=FALSE, trim=TRUE))
 }
